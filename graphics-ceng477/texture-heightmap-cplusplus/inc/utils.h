9b46ec6acdc6bb58fe2b114f803a186a458db70e
Kudaiar Pirimbaev <kudayar.pirimbaev@gmail.com>
2015-08-15 14:12:31 +0600

    Create utils.h

---
+++ graphics-ceng477/texture-heightmap-cplusplus/inc/utils.h
@@ -0,0 +1,28 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <unistd.h>
+#include <sys/time.h>
+#include <GL/gl.h>   
+#include <GL/glut.h> 
+#include "glentry.h"
+
+#include "math.h"
+
+using namespace std;
+
+
+
+GLuint createVS(const string& filename);
+
+GLuint createFS(const string& filename);
+
+bool readDataFromFile(const string& fileName, string &data);
+
+long getCurrentTime();
+
+#endif
